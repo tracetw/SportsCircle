d3378f6ebf431587b11689dc08ae5b01e4baf610
Charles Wang <charlestw611@gmail.com>
2015-07-28 13:42:58 +0800

    新增PostMainViewController 發文頁面.m.h檔

---
+++ SportsCircle/SportsCircle/PostMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostMainViewController.h
+//  SportsCircle
+//
+//  Created by Charles Wang on 2015/7/28.
+//  Copyright (c) 2015年 SportsCircle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostMainViewController : UIViewController
+
+@end
