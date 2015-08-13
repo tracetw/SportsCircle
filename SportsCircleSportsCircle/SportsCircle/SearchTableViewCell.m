35da8b2cd28f1edfd35c481eaec70a32bf081879
劉瑋軒 <vito5314@hotmail.com>
2015-08-13 13:09:36 +0800

    搜索連結個人頁面

---
+++ SportsCircleSportsCircle/SportsCircle/SearchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.m
+//  SportsCircle
+//
+//  Created by 劉瑋軒 on 2015/8/12.
+//  Copyright (c) 2015年 SportsCircle. All rights reserved.
+//
+
+#import "SearchTableViewCell.h"
+
+@implementation SearchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
