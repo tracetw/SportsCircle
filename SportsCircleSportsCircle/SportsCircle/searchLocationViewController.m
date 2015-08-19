ab072f65eb9cb5fe091b07de73d19a39c4abcdf9
劉瑋軒 <vito5314@hotmail.com>
2015-08-19 20:39:18 +0800

    bugfixed

--- SportsCircleSportsCircle/SportsCircle/searchLocationViewController.m
+++ SportsCircleSportsCircle/SportsCircle/searchLocationViewController.m
@@ -13,7 +13,7 @@
 
 #define WS(weakSelf)  __weak __typeof(&*self)weakSelf = self
 
-//static NSTimeInterval const kTimeDelay = 2.5;
+static NSTimeInterval const kTimeDelay = 2.5;
 
 
 @interface searchLocationViewController ()<CLLocationManagerDelegate,MKMapViewDelegate,UITextFieldDelegate>
