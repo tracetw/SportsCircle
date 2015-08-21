1d151868ac1c5ea9a70e45251727bf7d1913cf66
劉瑋軒 <vito5314@hotmail.com>
2015-08-21 21:27:16 +0800

    update

--- SportsCircleSportsCircle/SportsCircle/TrendViewController.m
+++ SportsCircleSportsCircle/SportsCircle/TrendViewController.m
@@ -98,7 +98,7 @@
     //[cell.contentView.layer setBorderColor:[UIColor redColor].CGColor];
     //[cell.contentView.layer setBorderWidth:10.0];
     
-
+    self.tableView.separatorColor=[UIColor whiteColor];
 }
 -(void)reloadDatas
 {
@@ -282,6 +282,8 @@
         
         cell.userImage.image = userImage.image;
         
+        [cell.contentView.layer setBorderColor:[UIColor whiteColor].CGColor];
+        [cell.contentView.layer setBorderWidth:8.0f];
         
     }];
     
